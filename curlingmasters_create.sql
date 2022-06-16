d6877ca39027772075dc3d104a3103b7dd7d313c
Asia Wojciechowicz <asia_wojciechowicz@MacBook-Pro-Asia.local>
2022-06-16 19:35:16 +0200

    fill database

--- curlingmasters_create.sql
+++ curlingmasters_create.sql
@@ -1,19 +1,23 @@
 SET FOREIGN_KEY_CHECKS = 0;
-CREATE OR replace SCHEMA CurlingMasters;
+CREATE OR replace SCHEMA team9;
 
-USE CurlingMasters;
+USE team9;
 
-CREATE TABLE FutureMatches
+CREATE TABLE CashFlow
 (
-  FutureID INT      NOT NULL AUTO_INCREMENT,
-  Date     DATETIME NOT NULL,
-  Team1ID  INT      NOT NULL,
-  Team2ID  INT      NOT NULL,
-  PRIMARY KEY (FutureID)
+  PaymentID      INT          NOT NULL AUTO_INCREMENT,
+  Date           DATE         NOT NULL,
+  Amount         DECIMAL(9,2) NOT NULL,
+  PlayerID       INT          NULL    ,
+  StaffID        INT          NULL    ,
+  ItemID         INT          NULL    ,
+  SponsorID      INT          NULL    ,
+  OtherPaymentID INT          NULL    ,
+  PRIMARY KEY (PaymentID)
 );
 
-ALTER TABLE FutureMatches
-  ADD CONSTRAINT UQ_FutureID UNIQUE (FutureID);
+ALTER TABLE CashFlow
+  ADD CONSTRAINT UQ_PaymentID UNIQUE (PaymentID);
 
 CREATE TABLE Info
 (
@@ -33,51 +37,40 @@ CREATE TABLE Info
 ALTER TABLE Info
   ADD CONSTRAINT UQ_InfoID UNIQUE (InfoID);
 
-CREATE TABLE ItemCosts
-(
-  PaymentID INT          NOT NULL AUTO_INCREMENT,
-  Date      DATE         NOT NULL,
-  Amount    DECIMAL(8,2) NOT NULL,
-  PRIMARY KEY (PaymentID)
-);
-
-ALTER TABLE ItemCosts
-  ADD CONSTRAINT UQ_PaymentID UNIQUE (PaymentID);
-
 CREATE TABLE Items
 (
-  ItemID    INT          NOT NULL AUTO_INCREMENT,
-  Size      VARCHAR(20)  NOT NULL,
-  Type      VARCHAR(128) NOT NULL,
-  Broken    BOOLEAN      NOT NULL,
-  PaymentID INT          NOT NULL,
+  ItemID      INT          NOT NULL AUTO_INCREMENT,
+  Size        VARCHAR(20)  NOT NULL,
+  Description VARCHAR(128) NOT NULL,
+  Price       DECIMAL(9,2) NOT NULL,
+  Quantity    INT          NOT NULL,
   PRIMARY KEY (ItemID)
 );
 
 ALTER TABLE Items
   ADD CONSTRAINT UQ_ItemID UNIQUE (ItemID);
 
-CREATE TABLE MatchesPlayed
+CREATE TABLE Matches
 (
-  MatchID      INT          NOT NULL AUTO_INCREMENT,
-  TicketSales  DECIMAL(8,2) NOT NULL,
-  Season       VARCHAR(20)  NOT NULL,
-  Date         DATETIME     NOT NULL,
-  Score        VARCHAR(20)  NOT NULL,
-  WinnerTeamID INT          NOT NULL,
-  LoserTeamID  INT          NOT NULL,
+  MatchID    INT         NOT NULL AUTO_INCREMENT,
+  Spectators INT         NULL    ,
+  Season     VARCHAR(20) NOT NULL,
+  Date       DATETIME    NOT NULL,
+  ScoreA     INT         NULL    ,
+  ScoreB     INT         NULL    ,
+  TeamA      INT         NOT NULL,
+  TeamB      INT         NOT NULL,
   PRIMARY KEY (MatchID)
 );
 
-ALTER TABLE MatchesPlayed
+ALTER TABLE Matches
   ADD CONSTRAINT UQ_MatchID UNIQUE (MatchID);
 
 CREATE TABLE OtherCosts
 (
   OtherPaymentID INT          NOT NULL AUTO_INCREMENT,
-  Date           DATE         NOT NULL,
-  Amount         DECIMAL(8,2) NOT NULL,
   Description    VARCHAR(128) NOT NULL,
+  Periodic       BOOLEAN      NOT NULL,
   PRIMARY KEY (OtherPaymentID)
 );
 
@@ -86,94 +79,89 @@ ALTER TABLE OtherCosts
 
 CREATE TABLE OutsideIncome
 (
-  IncomeID INT                   NOT NULL AUTO_INCREMENT,
-  Amount   DECIMAL(8,2)          NOT NULL,
-  Source   ENUM('Private','Gov') NOT NULL,
-  Date     DATE                  NOT NULL,
-  PRIMARY KEY (IncomeID)
+  SponsorID   INT          NOT NULL AUTO_INCREMENT,
+  SponsorName VARCHAR(128) NOT NULL,
+  Amount      DECIMAL(9,2) NOT NULL,
+  StartDate   DATE         NOT NULL,
+  EndDate     DATE         NOT NULL,
+  PRIMARY KEY (SponsorID)
 );
 
 ALTER TABLE OutsideIncome
-  ADD CONSTRAINT UQ_IncomeID UNIQUE (IncomeID);
+  ADD CONSTRAINT UQ_SponsorID UNIQUE (SponsorID);
+
+CREATE TABLE PlayerMatchPerformance
+(
+  Effectiveness DECIMAL(8,2) NULL    ,
+  Position      VARCHAR(128) NOT NULL,
+  MatchID       INT          NOT NULL,
+  PlayerID      INT          NOT NULL
+);
 
 CREATE TABLE Players
 (
-  PlayerID INT NOT NULL AUTO_INCREMENT,
-  TeamID   INT NOT NULL,
-  InfoID   INT NOT NULL,
+  PlayerID INT     NOT NULL AUTO_INCREMENT,
+  TeamID   INT     NOT NULL,
+  Active   BOOLEAN NOT NULL,
+  InfoID   INT     NOT NULL,
   PRIMARY KEY (PlayerID)
 );
 
 ALTER TABLE Players
   ADD CONSTRAINT UQ_PlayerID UNIQUE (PlayerID);
 
-CREATE TABLE PlayerTuition
-(
-  TuitionID INT          NOT NULL AUTO_INCREMENT,
-  Date      DATETIME     NOT NULL,
-  Amount    DECIMAL(8,2) NOT NULL,
-  PlayerID  INT          NOT NULL,
-  PRIMARY KEY (TuitionID)
-);
-
-ALTER TABLE PlayerTuition
-  ADD CONSTRAINT UQ_TuitionID UNIQUE (TuitionID);
-
-CREATE TABLE Salaries
-(
-  SalaryID   INT          NOT NULL AUTO_INCREMENT,
-  Date       DATE         NOT NULL,
-  Amount     DECIMAL(8,2) NOT NULL,
-  EmployeeID INT          NOT NULL,
-  PRIMARY KEY (SalaryID)
-);
-
-ALTER TABLE Salaries
-  ADD CONSTRAINT UQ_SalaryID UNIQUE (SalaryID);
-
 CREATE TABLE Staff
 (
-  EmployeeID INT          NOT NULL AUTO_INCREMENT,
-  Position   VARCHAR(128) NOT NULL,
-  InfoID     INT          NOT NULL,
-  PRIMARY KEY (EmployeeID)
+  StaffID  INT          NOT NULL AUTO_INCREMENT,
+  Position VARCHAR(128) NOT NULL,
+  Active   BOOLEAN      NOT NULL,
+  InfoID   INT          NOT NULL,
+  PRIMARY KEY (StaffID)
 );
 
 ALTER TABLE Staff
-  ADD CONSTRAINT UQ_EmployeeID UNIQUE (EmployeeID);
+  ADD CONSTRAINT UQ_StaffID UNIQUE (StaffID);
 
 CREATE TABLE Teams
 (
-  TeamID   INT                   NOT NULL AUTO_INCREMENT,
-  TeamName VARCHAR(100)          NOT NULL,
-  Type     ENUM('MALE','FEMALE') NOT NULL,
+  TeamID      INT                        NOT NULL AUTO_INCREMENT,
+  TeamName    VARCHAR(100)               NOT NULL,
+  Type        ENUM('MALE','FEMALE')      NOT NULL,
+  AgeCategory ENUM('JUNIORS', 'SENIORS') NOT NULL,
   PRIMARY KEY (TeamID)
 );
 
 ALTER TABLE Teams
   ADD CONSTRAINT UQ_TeamID UNIQUE (TeamID);
 
-ALTER TABLE Teams
-  ADD CONSTRAINT UQ_TeamName UNIQUE (TeamName);
+ALTER TABLE PlayerMatchPerformance
+  ADD CONSTRAINT FK_Matches_TO_PlayerMatchPerformance
+    FOREIGN KEY (MatchID)
+    REFERENCES Matches (MatchID);
 
-ALTER TABLE FutureMatches
-  ADD CONSTRAINT FK_Teams_TO_FutureMatches
-    FOREIGN KEY (Team1ID)
+ALTER TABLE PlayerMatchPerformance
+  ADD CONSTRAINT FK_Players_TO_PlayerMatchPerformance
+    FOREIGN KEY (PlayerID)
+    REFERENCES Players (PlayerID);
+
+ALTER TABLE Staff
+  ADD CONSTRAINT FK_Info_TO_Staff
+    FOREIGN KEY (InfoID)
+    REFERENCES Info (InfoID);
+
+ALTER TABLE Players
+  ADD CONSTRAINT FK_Info_TO_Players
+    FOREIGN KEY (InfoID)
+    REFERENCES Info (InfoID);
+
+ALTER TABLE Matches
+  ADD CONSTRAINT FK_Teams_TO_Matches
+    FOREIGN KEY (TeamB)
     REFERENCES Teams (TeamID);
 
-ALTER TABLE FutureMatches
-  ADD CONSTRAINT FK_Teams_TO_FutureMatches1
-    FOREIGN KEY (Team2ID)
-    REFERENCES Teams (TeamID);
-
-ALTER TABLE MatchesPlayed
-  ADD CONSTRAINT FK_Teams_TO_MatchesPlayed
-    FOREIGN KEY (WinnerTeamID)
-    REFERENCES Teams (TeamID);
-
-ALTER TABLE MatchesPlayed
-  ADD CONSTRAINT FK_Teams_TO_MatchesPlayed1
-    FOREIGN KEY (LoserTeamID)
+ALTER TABLE Matches
+  ADD CONSTRAINT FK_Teams_TO_Matches1
+    FOREIGN KEY (TeamA)
     REFERENCES Teams (TeamID);
 
 ALTER TABLE Players
@@ -181,27 +169,27 @@ ALTER TABLE Players
     FOREIGN KEY (TeamID)
     REFERENCES Teams (TeamID);
 
-ALTER TABLE PlayerTuition
-  ADD CONSTRAINT FK_Players_TO_PlayerTuition
+ALTER TABLE CashFlow
+  ADD CONSTRAINT FK_Players_TO_CashFlow
     FOREIGN KEY (PlayerID)
     REFERENCES Players (PlayerID);
 
-ALTER TABLE Players
-  ADD CONSTRAINT FK_Info_TO_Players
-    FOREIGN KEY (InfoID)
-    REFERENCES Info (InfoID);
+ALTER TABLE CashFlow
+  ADD CONSTRAINT FK_Staff_TO_CashFlow
+    FOREIGN KEY (StaffID)
+    REFERENCES Staff (StaffID);
 
-ALTER TABLE Staff
-  ADD CONSTRAINT FK_Info_TO_Staff
-    FOREIGN KEY (InfoID)
-    REFERENCES Info (InfoID);
+ALTER TABLE CashFlow
+  ADD CONSTRAINT FK_Items_TO_CashFlow
+    FOREIGN KEY (ItemID)
+    REFERENCES Items (ItemID);
 
-ALTER TABLE Salaries
-  ADD CONSTRAINT FK_Staff_TO_Salaries
-    FOREIGN KEY (EmployeeID)
-    REFERENCES Staff (EmployeeID);
+ALTER TABLE CashFlow
+  ADD CONSTRAINT FK_OutsideIncome_TO_CashFlow
+    FOREIGN KEY (SponsorID)
+    REFERENCES OutsideIncome (SponsorID);
 
-ALTER TABLE Items
-  ADD CONSTRAINT FK_ItemCosts_TO_Items
-    FOREIGN KEY (PaymentID)
-    REFERENCES ItemCosts (PaymentID);
+ALTER TABLE CashFlow
+  ADD CONSTRAINT FK_OtherCosts_TO_CashFlow
+    FOREIGN KEY (OtherPaymentID)
+    REFERENCES OtherCosts (OtherPaymentID);
